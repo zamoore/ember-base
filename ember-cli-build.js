c8bc722a16260cb3d8669eab4aa88bd3e995e7f4
Zack <zack@zacks-mbp.lan>
2022-05-14 16:59:51 -0400

    added tailwind

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,24 +1,29 @@
 'use strict';
 
+const autoprefixer = require('autoprefixer');
+const tailwind = require('tailwindcss');
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    postcssOptions: {
+      compile: {
+        cacheInclude: [/.*\.(css|hbs|ts|js)$/, /.tailwind\/config\.js$/],
+        plugins: [
+          {
+            module: autoprefixer,
+            options: {},
+          },
+          {
+            module: tailwind,
+            options: {
+              config: './app/styles/tailwind/config.js',
+            },
+          },
+        ],
+      },
+    },
   });
-
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
 
   return app.toTree();
 };
